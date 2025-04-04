48a465351adcaefd8b9d919fa87afbba3a332342
PabloLim4 <91170398+PabloLim4@users.noreply.github.com>
2025-04-03 22:19:16 -0300

    Att 03/04/25

--- src/pages/specialty/SpecialtyFormPage.vue
+++ src/pages/specialty/SpecialtyFormPage.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { DefaultTemplate } from '@/template'
+import { mdiCancel, mdiPlusCircle } from '@mdi/js'
+
 </script>
 
 <template>
@@ -8,6 +10,11 @@ import { DefaultTemplate } from '@/template'
       <h1>Specilaty Form</h1>
     </template>
 
-    <template #default> Meu formulário dgdgfgde especialidade ficará aqui ;) </template>
+    <template #default> Meu formulário de especialidade ficará aqui ;) </template>
+
+    <template #action>
+      <v-btn :prepend-icon="mdiCancel" :to="{ name: 'status-list' }"> Cancelar </v-btn>
+    
+    </template>
   </default-template>
 </template>
